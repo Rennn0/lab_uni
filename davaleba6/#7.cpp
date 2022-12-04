acd880ac9ab99f1106dcef49cbc36aa3fc3b3137
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-12-04 08:02:13 +0000

    ..

--- davaleba6/#7.cpp
+++ davaleba6/#7.cpp
@@ -14,17 +14,97 @@
 // სადაც მხოლოდ წაკითხვა გჭირდებათ, მიუთითეთ const.
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 #include <random>
 using namespace std;
 
+#define total   20
+#define row     4
+#define column  4
+
 random_device rnd;
 default_random_engine gen(rnd());
-uniform_int_distribution<int> dis(-1,1);
+uniform_real_distribution<double> dis(-0.5,0.5);
+/*
+    monacemis struqtura aseti iqneba
+    
+    yoveli wina dgis fasi iqneba momdevno dgis saangarishi X mnishvneloba
+    mesame wevrad random mnishvneloba sheinaxeba
+    [
+        [ [x(r), y0 ,r , ry] [x=y0, y1 , r, ry] [x=y1, y2 , r, ry ] [x=y2, y3 ,r, ry] ]
+        
+        [ [] [] [] [] ]
+        
+        [ [] [] [] [] ] 
+        .....
+    ]
 
+    grafiki y=1.05x+0.25
+    random diapazoni [-0.5,0.5]
+*/
+typedef double dimensions[row][column];
 
+void filler(dimensions *); // pointeri heapis pointerze, sawyisi X mnishvnelobebistvis
+void dependence(dimensions *); // y mnishnvelobebistvis
+void print(const dimensions *); // yvelafers gamoitans ekranze
+double calc(const double); 
 
 int main()
 {
-    int *ptr=new int[4];
-    
+    /* 
+        sul 20 dakvirveba
+        titoeulshi 4 dgis monacemebi
+        tito dges x y  random mnishvnelobeba da
+        random mnishvnelobis mixedvit y
+    */
+    dimensions *ptr=new dimensions[total];
+    filler(ptr);
+    dependence(ptr);
+    print(ptr);
+        
+}
+
+void filler(dimensions *p) // yoveli arrays pirveli X wevri xdeba rand
+{
+    for(size_t t=0; t<total; t++)
+        {
+            *(*(*(p+t)))=rand()%3-1; // sawyisi mnishvneloba [-1,1] shauledidan
+            *(*(*(p+t))+1)=calc(*(*(*(p+t)))); // pirveli dgis aqciis fasi
+        }
+}   
+
+void dependence(dimensions *p)
+{
+    for(size_t t=0; t<total; t++)
+        for(size_t r=1; r<row; r++) // pirveli dgis 
+            {
+                *(*(*(p+t)+r))=*(*(*(p+t)+r-1)+1);
+                *(*(*(p+t)+r)+1)=calc(*(*(*(p+t)+r))); 
+                *(*(*(p+t)+r)+2)=dis(gen);
+            }
+
+}
+
+//  y=1.05x+0.25
+double calc(const double x)
+{
+    return 1.05 * x + 0.25;
+}
+
+void print(const dimensions *p)
+{
+    for(size_t t=0; t<total; t++)
+    {
+        cout<<"\nDay "<<t+1<<"\nX\t\tY(f(x))\t\tRandom\t\tY(Random)";
+        for(size_t r=0; r<row; r++)
+        {
+        cout<<endl;
+            for(size_t c=0; c<column; c++)
+            {
+                cout<<fixed<<setprecision(4)<<*(*(*(p+t)+r)+c)<<"\t\t";
+            }
+        }
+        cout<<endl;
+    }
 }
