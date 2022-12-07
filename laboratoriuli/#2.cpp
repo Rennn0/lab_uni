daa23cb7fedd5cbf214e28e7a24c462aba05af81
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-12-07 12:26:01 +0000

    pointerebia gasasworebeli

---
+++ laboratoriuli/#2.cpp
@@ -0,0 +1,25 @@
+#include "../lib.h"
+
+void swap(char**,char**);
+void SRT(char**,char**);
+
+int main(int argc, char** argv)
+{
+    cout<<"Before _ "<<argv[1]<<" "<<argv[2]<<endl;
+    swap(argv[1],argv[2]);
+    cout<<"After _ "<<argv[1]<<" "<<argv[2]<<endl;
+    SRT(&argv[1],&argv[2]);
+    // cout<<"After Sort_ "<<argv[1]<<" "<<argv[2]<<endl;
+}
+
+void swap(char** s1,char** s2)
+{
+    char* temp=*s1;
+    s1=s2;
+    *s2=temp;
+}
+
+void SRT(char** s1,char** s2)
+{
+   cout<<s1;
+}
