3d612c4b549afcc800d46681a25bb1684a1ce42c
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-12-07 18:05:28 +0000

    kanfeeeet

--- laboratoriuli/#2.cpp
+++ laboratoriuli/#2.cpp
@@ -1,15 +1,17 @@
 #include "../lib.h"
 
 void swap(char**,char**);
-void SRT(char**,char**);
+void SRT(char*,char*);
 
 int main(int argc, char** argv)
 {
     cout<<"Before _ "<<argv[1]<<" "<<argv[2]<<endl;
+
     swap(argv[1],argv[2]);
     cout<<"After _ "<<argv[1]<<" "<<argv[2]<<endl;
-    SRT(&argv[1],&argv[2]);
-    // cout<<"After Sort_ "<<argv[1]<<" "<<argv[2]<<endl;
+    
+    SRT(argv[1],argv[2]);
+    cout<<"After Sort_ "<<argv[1]<<" "<<argv[2]<<endl;
 }
 
 void swap(char** s1,char** s2)
@@ -19,7 +21,8 @@ void swap(char** s1,char** s2)
     *s2=temp;
 }
 
-void SRT(char** s1,char** s2)
+void SRT(char* s1,char* s2)
 {
-   cout<<s1;
+   sort(s1,s1+strlen(s1));
+   sort(s2,s2+strlen(s2));
 }
