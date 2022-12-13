afa889157f846f2f496be87ad7d20f94a70ff7a2
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-12-13 14:15:40 +0400

    Add files via upload

---
+++ maglivi/Black_scholes_model/Black_scholes_model.cpp
@@ -0,0 +1,231 @@
+﻿#include "class.h"
+#define CLEAR cout<<"\033[2J"; // ეკრანს გაწმენდს
+#define BEGIN cout<<"\033[1;1H"; // კურსორს სათავეში დააბრუნებს
+
+vector<Bsm>* DATA = new vector<Bsm>; // კლასების ვექტორი
+
+void greet(); // მისალმება
+void bsm_call(); // პარამეტრიან კლასს გამოიძახებს
+void bsm_call_r(); // რანდომ მნიშვნელობებიან კლასს გააკეთებს
+void opp(); // ოპერაციების მენიუ
+void print_all(); // მთლიანი  კლასების ვექტორის დასაბეჭდად
+void new_obj(); // კლასის პარამეტრიან კონსტრუქტორს გამოძახებს
+
+
+int main()
+{
+    greet();
+    char question; cin >> question; // ოპერაციის ბრძანების მისაღებად
+g:
+    if (question == 'y') // დავიწყოთ
+    {
+        CLEAR; BEGIN; opp();//ოეპრაციებს დაბეჭდავს
+
+        while (cin >> question) // ბრძანებების მისაღებად
+        {
+            CLEAR; BEGIN; opp();
+            switch (question) // სვიჩი ინფუთის შესაბამისად
+            {
+            case 'r': // რანდომ  აქცია
+                bsm_call_r(); cout << endl << '_';
+                break;
+            case 'p': // გამოთვლის სიმულატორში გადავა
+                CLEAR; BEGIN;
+                bsm_call();
+                opp();
+                break;
+            case 'd': // ინდექსს მოსთხოვს
+                int id;
+                cout << "\nID="; cin >> id;
+                if (DATA->size() < id) // სისწორის შემოწმება,იდ [1,ზომა) ფარგლებშია
+                {
+                    cout << "\nobieqti mititebuli ID nomrit ar arsebobs\n_"; break;
+                }
+                cout << DATA->at(id - 1);
+                break;
+            case 'a':
+                if (DATA->size() == 0) // შეამოწმებს არის თუარა წევრები ვექტორში
+                {
+                    cout << "araferia dasabechdi ://\n_"; break;
+                }
+                print_all(); // ყველას დაბეჭდავს
+                break;
+            case 's': // შედარების ბლოკი
+                char c; CLEAR; BEGIN;
+                cout << "ukve arsebulis tu axals gaaketeb (u/a)?"; cin >> c; // ორი არჩევანი
+                if (c == 'u' && DATA->size() >= 2) //თუ ძველი უნდა + ბაზაში 2 ელემენტი მაინცაა 
+                {
+                    int id1, id2; // ორი ელემენტის აიდი
+                    bool z; // > < = შემოწმებისთვის
+                    cout << "ID1="; // პირველი ობიექტის აიდი
+                    while (!(cin >> id1) || id1 > DATA->size()) // აიდების სისწორის შემოწმება
+                    {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "araswori ID\n_";
+                    }
+                    cout << "ID2="; // მეორე ობიექტის აიდი
+                    while (!(cin >> id2) || id2 > DATA->size()) // ბოლო აიდის ნომერი ვექტორის სიგრძეა, ანუ მაგაზე მეტი ინფუთი არუნდაიყოს
+                    {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "araswori ID\n_";
+                    }
+                    cout << DATA->at(id1 - 1) << DATA->at(id2-1); // დაბეჭდავს ობიექტებს 
+                    z = DATA->at(id1 - 1) > DATA->at(id2 - 1); // შედარებისთვის
+                    if (z == true) // მარტივი იფ ელსი
+                    {
+                        cout << "\n\t\tobj ID" << id1 << "> ID" << id2 << '\n';
+                    }
+                    else if (z == false)
+                    {
+                        cout << "\n\t\tobj ID" << id1 << "< ID" << id2 << '\n';
+                    }
+                    else
+                    {
+                        cout << "\n\t\torive tolia\n";
+                    }
+                    opp(); // მენიუს გამოიტანს ისევ
+                    cout << endl << '_';
+                    break;
+                }
+                else if (c == 'a') // თუ ახალი უნდა
+                {
+                    CLEAR; BEGIN;
+
+                    cout << "\npirveli obieqtebis shesaqmnelad miyevit instruqciebs:\n";
+                    new_obj(); // პარამეტრიანი კონსტრუქტორი
+
+                    cout << "\nmeore obieqtebis shesaqmnelad miyevit instruqciebs:\n";
+                    new_obj();
+
+                    cout << DATA->at(DATA->size() - 2) << DATA->at(DATA->size() - 1);
+                    bool z = DATA->at(DATA->size() - 2) > DATA->at(DATA->size() - 1);
+
+                    // უტოლობის გამოტანა ზევითას მსგავსად შემეძლო თუმცა ასე დავტოვებ
+                    cout << endl << boolalpha << z << endl; 
+                    // true მხოლოდ მეტობის შემთხვევაში იქნება
+                    opp();
+                    cout << endl << '_';
+                    break;
+                }
+                else // თუ არც ძველი უნდა არც ახალი, ნუ რაღაც სხვა დაწერა
+                {
+                    cout << "araswori brdzaneba!";
+                    opp();
+                    cout << endl << '_';
+                    break;
+                }
+            case '!': // პროგრამა გაჩერდება
+                CLEAR; BEGIN;
+                cout << "\nbye bye\n";
+                exit(0);
+            default: // შეცდომის დამფიქსირებელი, ანუ  შემოტანილი ჩარ არაფერს ემთხვევა
+                cout << "\naraswori operacia\n";
+            }
+        }
+    }
+    else if (question == 'n') // არ დავუწყით
+    {
+        cout << "\n:(((((((((((((\n";
+        exit(1);
+    }
+    else // რაღაც განსხვავებული დაიბეჭდა
+    {
+        cout << "\nragac shegeshala :| ";
+        cin >> question;
+        goto g; // ეს შეკითხვის რეჟიმში გადაიყვანს ისევ
+    }
+}
+
+void greet() // მისალმების ფუნქცია
+{
+    string left(51, '#'), h(20, '-');
+    cout << left << endl << h << "->Welcome<-" << h << endl << left << endl;
+    cout << "\nPrograma Black_Scholes_Model Gamotvlebistvis\n"
+        "daviwyot?? (y/n) _ ";
+}
+void bsm_call() // ჰიპზე შექმნილი კლასი შემდეგ ჰიპის ვექტორზევე იფუშება
+{
+    Bsm* ptr = new Bsm;
+    cin >> *ptr;
+    DATA->push_back(*ptr);
+    cout << *ptr;
+}
+
+void bsm_call_r() // იგივე აქაც,ოღონდ რანდომ მნიშვნელობით
+{
+    Bsm* ptr = new Bsm;
+    DATA->push_back(*ptr);
+    cout << *ptr;
+}
+
+void opp() { // მენიუ
+    cout << "\nShesadzlo operaciebi:\n\n";
+    cout << left << setw(40) << "random BSM_ " << "r\n";
+    cout << left << setw(40) << "parametriani BSM_ " << "p\n";
+    cout << left << setw(40) << "dabechdva ID mixedvit_" << "d\n";
+    cout << left << setw(40) << "yvelafris dabechdva_" << "a\n";
+    cout << left << setw(40) << "shedareba aqciebis_" << "s\n";
+    cout << left << setw(40) << "programis shesawyvetad_ " << "!\n";
+}
+
+void print_all()// ყველაფრის ბეჭდვა
+{
+    for (auto v : *DATA)
+        v.print(); // ობიექტის პრინტ ფუნქციას გამოვიყენებ
+}
+
+void new_obj()
+{
+    double s, k, r, t, u; // ხუთივე პარამეტრი, შემდეგ სისწორის შემოწმებებია ყველასთვის
+    cout << "Stock Price_";
+    while (!(cin >> s) || s<0) 
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "error\n_";
+    }
+    cout << "strike Price_"; 
+    while (!(cin >> k) || s<0)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "error\n_";
+    }
+    cout << "risk free interest rate_"; 
+    while (!(cin >> r) || r<0 || r>100)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "error\n_";
+    }
+    cout << "time_"; 
+    while (!(cin >> t) || t<0)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "error\n_";
+    }
+    cout << "volatility_";
+    while (!(cin >> u) || u<0 || u>100)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "error\n_";
+    }
+    // ვექტორში ახალი ობიექტი დაიფუშება
+    DATA->push_back(Bsm(s, k, r, t, u)); CLEAR; BEGIN;
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ maglivi/Black_scholes_model/class.h
@@ -0,0 +1,179 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <random>
+#include <limits>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+#define e 2.71828
+#define p 3.14159
+
+/*Black Scholes Model matematikuri formula
+C=SN(d1)- Ke^(-rt)N(d2)
+    sadac
+    d1=(ln_k_(S) + (r + (u^2)/2)t) / u(s)t^1/2
+    d2=d1-u(s)t^1/2
+C_call option price
+// useris shemosatani mnishvnelobebi klasistvis
+S_current stock
+K_strike price
+r_risk free interest rate
+t_time to maturity
+u_volatility
+
+დეტალურად  ბოლოში
+
+გამოთვლებში სხვაობაა ონლაინ კალკულატორთან შედარებით
+ფორმულა იგივეა , დივიდენდებს არ ვიყენებ  მხოლოდ,
+შეცდომას ვერ ვხედავ, ერთი ვარიანტი N  განაწილების გამოთვლაშია
+ხარვეზი სავარაუდოდ
+*/
+random_device rnd; // რანდომ მნიშვნელობებს ამოყრის კლასისთვის
+default_random_engine gen(rnd());
+uniform_real_distribution<double>dis(0, 100);
+
+int ID_count = 1; // გლობალური აიდი
+
+class Bsm
+{
+private:
+    double S, K, R, T, U, result; // რესულტ  ბოლოს დაითვლება და მიენიჭება მნიშვნელობა ავტომაურად
+    int ID; // ყველა ობიექტს ინდივიდუალური აიდი ექნება
+public:
+    Bsm() { ID = ID_count++; S = dis(gen), K = dis(gen), R = dis(gen), T = dis(gen), U = dis(gen), call_opt_price(); } // რანდომ მნიშვნელობები მიენიჭებათ და რესულტიც ესე დაიტვლება
+    Bsm(double s, double k, double r, double t, double u)
+    {
+        ID = ID_count++; S = s, K = k, R = r, T = t, U = u, call_opt_price();
+    } // კლასის გამოცხადებისას მინიჭებული პარამეტრებით
+
+// wvdoma rom qonoda private wevrebze friend gavxade
+    friend ostream& operator << (ostream& os, const Bsm& obj);
+    friend istream& operator >> (istream& is, Bsm& obj);
+    friend bool operator < (const Bsm&, const Bsm&);
+    friend bool operator > (const Bsm&, const Bsm&);
+
+    // gadatvirtuli cout<< _is msgavsi iqneba 
+    void print()const;
+
+    // math go brrrr
+    double call_opt_price();
+};
+
+//ბეჭდვის ფუნქცია
+
+inline void Bsm::print()const
+{
+    string h(30, '*');
+    cout << h << endl;
+    cout << "Object ID_" << this->ID << endl;
+    cout << "Current stock_" << fixed << setprecision(2) << this->S << endl;
+    cout << "Strike price_" << fixed << setprecision(2) << this->K << endl;
+    cout << "Risk free interest rate_" << fixed << setprecision(4) << this->R << endl;
+    cout << "Time to maturity(y)_" << fixed << setprecision(2) << this->T << endl;
+    cout << "Volatility_" << fixed << setprecision(4) << this->U << endl;
+    cout << "Call option price_" << fixed << setprecision(3) << this->result << endl;
+}
+
+// gadavtvirtav cin cout operaciesbs klasis input outputistvis , ასევე < და   >
+istream& operator >> (istream& is, Bsm& obj)
+{
+    string h(30, '-');
+    double s, k, r, t, u;
+    cout << "\nBlack-Scholes Model simulator \n"
+        << h << "\nshemoitanet monacemebi gamotvlebistvis" << endl;
+
+    cout << left << setw(30) << "Current stock_";
+    // shezgudva daedeba yvelanair inputs garda double_sa
+    // 2saati am metodis dzebnas movundi -_____-
+
+    while (!(is >> s) || s < 0)
+    {
+        is.clear(); // buffers gaasuftavebs
+        is.ignore(numeric_limits<streamsize>::max(), '\n'); // gaabatilebs mtlian inputs sanam axali xazi ar shexvdeba
+        cout << "shemoitane validuri mnishvneloba_"; // shecdomis dzaxili, mere tavidan daawerinebs
+    }
+
+    cout << left << setw(30) << "Strike price_";
+    while (!(is >> k) || k < 0)
+    {
+        is.clear();
+        is.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "shemoitane validuri mnishvneloba_";
+    }
+
+    cout << left << setw(30) << "Risk free interest rate_";
+    while (!(is >> r) || r < 0 || r>100)
+    {
+        is.clear();
+        is.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "shemoitane validuri mnishvneloba_";
+    }
+
+    cout << left << setw(30) << "Time to maturity(y)_";
+    while (!(is >> t) || t < 0)
+    {
+        is.clear();
+        is.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "shemoitane validuri mnishvneloba_";
+    }
+
+    cout << left << setw(30) << "Volatility_";
+    while (!(is >> u) || u < 0)
+    {
+        is.clear();
+        is.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "shemoitane validuri mnishvneloba_";
+    }
+
+    obj.S = s; obj.K = k; obj.R = r; obj.T = t, obj.U = u; obj.call_opt_price();
+    // shemedzlo obj=Bsm(s,k,r,t,u) gameketebina da friend wvdoma ar damchirdeboda
+    // magram operacia axali obieqtis sheqmnad chaitvleboda da globalur ID awevda
+    // gadatvirtva razec gamovidzaxe im obieqtis IDc sheicvleboda
+    return is;
+}
+
+ostream& operator << (ostream& os, const Bsm& obj)
+{
+    string h(30, '*');
+
+    os << h << "\nObject ID_" << obj.ID << "\nmonacemebi:";
+    os << left << setw(30) << "\nCurrent stock_" << fixed << setprecision(2) << obj.S;
+    os << left << setw(30) << "\nStrike price_" << fixed << setprecision(2) << obj.K;
+    os << left << setw(30) << "\nRisk free interest rate_" << fixed << setprecision(4) << obj.R;
+    os << left << setw(30) << "\nTime to maturity(y)_" << fixed << setprecision(2) << obj.T;
+    os << left << setw(30) << "\nVolatility_" << fixed << setprecision(4) << obj.U;
+    os << left << setw(30) << "\nCall option price_" << fixed << setprecision(3) << obj.result << endl;
+    return os;
+}
+
+bool operator < (const Bsm& obj1, const Bsm& obj2)
+{
+    return obj1.result < obj2.result;
+}
+
+bool operator > (const Bsm& obj1, const Bsm& obj2)
+{
+    return obj1.result > obj2.result;
+}
+
+// გამოთვლა  საბოლოო მნიშვნელობის
+inline double Bsm::call_opt_price()
+{
+    double
+        d1 = (log10(S) - log10(K) + (R + U * U / 2) * T) / U / sqrt(T),
+        d2 = d1 - U * sqrt(T),
+        top1 = -(1 / 2) * pow(d1 / U, 2),
+        top2 = -(1 / 2) * pow(d2 / U, 2),
+        N1 = pow(e, top1) / U / sqrt(2 * p),
+        N2 = pow(e, top2) / U / sqrt(2 * p),
+
+        answer = S * N1 - K * pow(e, -R * T) * N2;
+
+    return result = answer;
+}
+
+
